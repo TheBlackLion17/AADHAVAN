5c355b647edaf5b8350c74226d04432a4746d155
Agsmods <136147260+Agsbotsyt@users.noreply.github.com>
2023-06-13 11:01:00 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -620,7 +620,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('SOURCE CODE', url='https://github.com/MrMKN/PROFESSOR-BOT')
+            InlineKeyboardButton('SOURCE CODE', url='https://github.com/Agsbotsyt/AADHAVAN')
             ],[
             InlineKeyboardButton('👩‍🦯 Back', callback_data='about')
         ]]
